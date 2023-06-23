68c0b8b4b61f00fc21fd5a578fe7a3bccd2389f7
eliezerantonio <eliezerfernandoantonio@gmail.com>
2023-06-23 16:33:20 +0100

    feat: module implementation

--- 02-base/src/app/app.module.ts
+++ 02-base/src/app/app.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { CounterComponent } from './counter/counter.component';
-import { HeroComponent } from './heroes/hero/hero.component';
-import { ListComponent } from './heroes/list/list.component';
+
+import { CounterModule } from './counter/counter.module';
+import { HeroesModule } from './heroes/heroes.module';
 
 @NgModule({
-  declarations: [AppComponent, CounterComponent, HeroComponent, ListComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent],
+  imports: [BrowserModule, CounterModule, HeroesModule],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ 02-base/src/app/counter/counter.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { CounterComponent } from './components/counter/counter.component';
+
+@NgModule({
+  declarations: [CounterComponent],
+  exports: [CounterComponent],
+})
+export class CounterModule {}
