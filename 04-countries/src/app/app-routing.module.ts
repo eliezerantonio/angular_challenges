521c5b4359f1d892fb4a13a0574ee852b0ffda87
eliezerantonio <eliezerfernandoantonio@gmail.com>
2023-08-10 11:58:04 +0100

    ✨ feat(router): use router-outlet to toggle routers

--- 04-countries/src/app/app-routing.module.ts
+++ 04-countries/src/app/app-routing.module.ts
@@ -19,5 +19,6 @@ const routes: Routes = [
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
 })
 export class AppRoutingModule {}
--- 04-countries/src/app/app.module.ts
+++ 04-countries/src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, SharedModule],
+  imports: [BrowserModule, SharedModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
