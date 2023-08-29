ed23e919b194efb3c441296cf55cb0084d2f25d7
eliezerantonio <eliezerfernandoantonio@gmail.com>
2023-08-29 01:18:31 +0100

    ✨ feat(country): search by country implementation

--- 04-countries/src/app/countries/pages/by-country-page/by-country-page.component.ts
+++ 04-countries/src/app/countries/pages/by-country-page/by-country-page.component.ts
@@ -1,8 +1,19 @@
 import { Component } from '@angular/core';
-
+import { CountriesService } from '../../services/countries.service';
+import { Country } from '../../interfaces/country';
+import { count } from 'rxjs';
 @Component({
   selector: 'app-by-country-page',
   templateUrl: './by-country-page.component.html',
   styles: [],
 })
-export class ByCountryPageComponent {}
+export class ByCountryPageComponent {
+  public countries: Country[] = [];
+  constructor(private countriesService: CountriesService) {}
+
+  public searchByCountry(name: string) {
+    this.countriesService.searchCountry(name).subscribe((countries) => {
+      this.countries = countries;
+    });
+  }
+}
