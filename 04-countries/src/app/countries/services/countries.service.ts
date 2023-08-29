b284693bde520c8d6b7dd178778c98f3a07cd544
eliezerantonio <eliezerfernandoantonio@gmail.com>
2023-08-29 01:17:12 +0100

    ✨ feat(countries services): add methods to get country and region

--- 04-countries/src/app/countries/services/countries.service.ts
+++ 04-countries/src/app/countries/services/countries.service.ts
@@ -1,15 +1,26 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, catchError, of } from 'rxjs';
 import { Country } from '../interfaces/country';
 
 @Injectable({ providedIn: 'root' })
-export class ContriesService {
+export class CountriesService {
   private apiUrl: string = 'https://restcountries.com/v3.1';
 
   constructor(private http: HttpClient) {}
 
   searchCapital(term: string): Observable<Country[]> {
-    return this.http.get<Country[]>(`${this.apiUrl}/capital/${term}`);
+    const url = `${this.apiUrl}/capital/${term}`;
+    return this.http.get<Country[]>(url).pipe(catchError((error) => of([])));
+  }
+
+  searchCountry(term: string): Observable<Country[]> {
+    const url = `${this.apiUrl}/name/${term}`;
+    return this.http.get<Country[]>(url).pipe(catchError((error) => of([])));
+  }
+
+  searchRegion(term: string): Observable<Country[]> {
+    const url = `${this.apiUrl}/region/${term}`;
+    return this.http.get<Country[]>(url).pipe(catchError((error) => of([])));
   }
 }
