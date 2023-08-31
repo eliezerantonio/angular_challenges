2f0574c8289f2f4141a78394b77aaeebf19e5dd8
eliezerantonio <eliezerfernandoantonio@gmail.com>
2023-08-31 21:28:31 +0100

    ✨ feat(heroes app): configure routes, child routes and lazyLoad

---
+++ 06-heroesApp/src/app/auth/auth-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { RegisterPageComponent } from './pages/register-page/register-page.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutPageComponent,
+
+    children: [
+      { path: 'login', component: LoginPageComponent },
+      { path: 'new-account', component: RegisterPageComponent },
+      { path: '**', redirectTo: 'login' },
+    ],
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+  providers: [],
+})
+export class AuthRoutingModule {}
---
+++ 06-heroesApp/src/app/shared/shared.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Error404PageComponent } from './pages/error404-page/error404-page.component';
+
+@NgModule({
+  declarations: [Error404PageComponent],
+  imports: [CommonModule],
+  exports: [Error404PageComponent],
+})
+export class SharedModule {}
